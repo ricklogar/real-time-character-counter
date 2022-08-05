3a220ea67e6cba34c6dad47d1fbbdb7249620e49
Ricardo Lopez <therlg13@gmail.com>
2022-08-04 22:08:07 -0500

    Javascript section

---
+++ index.js
@@ -0,0 +1,15 @@
+const textareaEl = document.getElementById("textarea");
+const totalCounterEl = document.getElementById("total-counter");
+const remainingCounterEl = document.getElementById("remaining-counter");
+
+textareaEl.addEventListener("keyup", ()=>{
+    console.log("key is pressed");
+    updateCounter();
+});
+
+updateCounter();
+
+function updateCounter(){
+    totalCounterEl.innerText = textareaEl.value.length;
+    remainingCounterEl.innerText = textareaEl.getAttribute("maxLength") - textareaEl.value.length;
+}
